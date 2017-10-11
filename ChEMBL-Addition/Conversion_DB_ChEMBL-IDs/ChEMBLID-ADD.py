ea75793aef90a24daa586e230ae1316764ee0868
Krish Bharat <krishbharat96@gmail.com>
2017-10-11 16:55:58 -0700

    Update ChEMBLID-ADD.py

--- ChEMBL-Addition/Conversion_DB_ChEMBL-IDs/ChEMBLID-ADD.py
+++ ChEMBL-Addition/Conversion_DB_ChEMBL-IDs/ChEMBLID-ADD.py
@@ -1,5 +1,10 @@
+# Algorithm for adding ChEMBL IDs to existing Compounds within SPOKE
+
 from neo4j.v1 import GraphDatabase, basic_auth
 import csv
+
+driver = GraphDatabase.driver("bolt://neo4j-server", auth=basic_auth("Username", "Password")) # Neo4j server details left out for security purposes
+session = driver.session()
 
 def add_string(parameter1, parameter2):
     return "MATCH (n:Compound {identifier: '" + parameter1 +  "'})" + " SET n.chembl_id='" + parameter2 + "'" + " RETURN n.name"
@@ -8,12 +13,10 @@ with open('DB-TO-CHEMBL-PROGRAM.csv') as f:
   reader = csv.reader(f, delimiter=',')
   for line in reader:
       if line:
-          doid = line[0]
+          dbid = line[0]
           chemblid = line[1]
-          q = add_string(str(doid), str(chemblid))
+          q = add_string(str(dbid), str(chemblid))
           print(q)  
-          driver = GraphDatabase.driver("bolt://127.0.0.1:7687", auth=basic_auth("neo4j", "tejas320"))
-#          driver = GraphDatabase.driver("bolt://msgap1.ucsf.edu:7687", auth=basic_auth("kbharat96", "tejas321"))
-          session = driver.session()
           session.run(q)
-          session.close()
+            
+session.close()
