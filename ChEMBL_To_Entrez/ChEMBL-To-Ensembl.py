0dd1f2a69bde9c4edbd49ae236f49f08e43a8b43
Krish Bharat <krishbharat96@gmail.com>
2017-10-11 23:20:36 -0700

    Update and rename GeneIDExtraction.py to ChEMBL-To-Ensembl.py

--- ChEMBL_To_Entrez/ChEMBL-To-Ensembl.py
+++ ChEMBL_To_Entrez/ChEMBL-To-Ensembl.py
@@ -1,5 +1,9 @@
+# Code for parsing the ChEMBL website for the Ensembl Gene ID
+# Note that this was mainly done for the previous SPOKE implementation in which there were only edges directly connecting Compounds to Genes
+# Refer to UniProt-Addition folder for more details regarding this protein node and edge addition
+
 import csv
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # HTML parsing package
 import requests
 import re
 
@@ -12,9 +16,8 @@ with open('targets.txt') as f:
     reader = csv.reader(f, delimiter = '\t')
     for line in reader:
         if line:
-            #print(line[1])
             print(line[5])
-            page = requests.get('https://www.ebi.ac.uk/chembl/target/inspect/' + line[5]).text
+            page = requests.get('https://www.ebi.ac.uk/chembl/target/inspect/' + line[5]).text # Parsing the ChEMBL site for the Ensembl ID
             soup = BeautifulSoup(page, "lxml")
             Ensembl = soup.body.find_all(text=re.compile('ENSG'))
             if Ensembl:
@@ -26,9 +29,6 @@ with open('targets.txt') as f:
                 varnf = commacatenate(line[5], "Not Found")
                 e.write("Not Found")
                 e.write('\n')
-                #var = commacatenate(line[5], i)
-                #print(var1)
-                #print(var)
             
                 
                 
