8b6eaaaddbbb6733c48ab5ef04cc7795c87d0002
Krish Bharat <krishbharat96@gmail.com>
2017-10-11 17:17:47 -0700

    Update and rename ChEMBL-Addition/stdSMILES-insert.py to ChEMBL-Addition/Standardization/stdSMILES-insert.py

--- ChEMBL-Addition/Standardization/stdSMILES-insert.py
+++ ChEMBL-Addition/Standardization/stdSMILES-insert.py
@@ -3,14 +3,15 @@ from neo4j.v1 import GraphDatabase, basic_auth
 def concatenate(chemblid, stdsmiles):
     return 'MATCH (n:Compound) where n.chembl_id = "' + chemblid + '" set n.standardized_smiles = "' + stdsmiles + '"'
 
+driver = GraphDatabase.driver("bolt:neo4j-server:7687", auth=basic_auth("Username", "Password"))
+session = driver.session()
+
 file = open('stdsmiles-atkins-all.csv', 'r')
 
 for line in file:
     if line:
         cols = line.split(',')
-        if not "None" in cols[1]:
-            driver = GraphDatabase.driver("bolt://msgap1.ucsf.edu/:7687", auth=basic_auth("kbharat96", "tejas320"))
-            session = driver.session()
+        if not "None" in cols[1]: # Some instances in which there were no SMILES identifiers from ChEMBL, and some nodes do not have SMILES on SPOKE - have to filter them out
             cmd = concatenate(cols[0], cols[2].strip())
             print cmd
             session.run(cmd)
