184b929b18a54dfb06d3a837d6b9da6ec3015603
Krish Bharat <krishbharat96@gmail.com>
2017-10-20 17:52:12 -0700

    Add files via upload

---
+++ Run.sh
@@ -0,0 +1,3 @@
+!#/bin/bash
+
+python ./create-SPOKE_Sources_DB.py;python ./create-Node_Props_Ref.py;python ./create-Rel_Props_Ref.py;python ./create-Ref_Source-key1.py;python ./create-Ref_Source-key2.py;python ./create-Ref_Source-key3.py;python ./create-Node_Keys_Sources.py;python ./create-Rel_Key_Sources1.py;python ./create-Rel_Key_Sources2.py;python ./create-SPOKE_Sources_Keys.py
