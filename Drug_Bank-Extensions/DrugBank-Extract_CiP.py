24668b102e052866b86f6c85e5ea8c89e9bb5ac6
Krish Bharat <krishbharat96@gmail.com>
2017-11-01 20:26:57 -0700

    Update DrugBank-Extract_CiP.py

--- Drug_Bank-Extensions/DrugBank-Extract_CiP.py
+++ Drug_Bank-Extensions/DrugBank-Extract_CiP.py
@@ -6,10 +6,19 @@ from bs4 import BeautifulSoup
 ## UniProt File Schema:
 ## ID,Name,Gene Name,GenBank Protein ID,GenBank Gene ID,UniProt ID,Uniprot Title,PDB ID,GeneCard ID,GenAtlas ID,HGNC ID,Species,Drug IDs
 
-file = open('all-protein-targets.csv', 'r') # Downloaded from DrugBank Website @ 
-op_file = open('prot-output.csv', 'w+')
-op_file.write('ProteinID,ProteinType,DrugBankID,DrugName,DrugGroup,Pharm_Action,Drug_Actions') # Schema of new file
+file = open('all-protein-targets-copy.csv', 'r') # Downloaded from DrugBank Website
+op_file = open('prot-cmpd-output-full-trial.csv', 'w+')
+op_file.write('ProteinID|ProteinType|DrugBankID|DrugName|DrugGroup|Pharm_Action|Drug_Actions')
 op_file.write('\n')
+
+def elim_split(param):
+    p1 = param.replace('<td>', '')
+    p2 = p1.replace('</td>', '')
+    p3 = p2.replace('</strong><strong class="badge">', ';')
+    p4 = p3.replace('<strong class="badge">', '')
+    p5 = p4.replace('</strong>', '')
+    return p5.strip()
+
 
 for line in file:
     cols = line.split(',')
@@ -27,11 +36,10 @@ for line in file:
                    dname = c[1].text
                    dgroup = c[2].text
                    dpharm_act = c[3].text
-                   dact = c[4].text
+                   dact = elim_split(str(c[4].encode('utf-8')))
                    print cols[0], 'target', dbid, dname, dgroup, dpharm_act, dact
-                   op_file.write(str(cols[0]).strip() + ',target,' + dbid.encode('utf-8') + ',' + dname.encode('utf-8') + ',' + dgroup.encode('utf-8') + ',' + dpharm_act.encode('utf-8') + ',' + dact.encode('utf-8'))
+                   op_file.write(str(cols[0]).strip() + '|target|' + dbid.encode('utf-8') + '|' + dname.encode('utf-8') + '|' + dgroup.encode('utf-8') + '|' + dpharm_act.encode('utf-8') + '|' + dact.encode('utf-8'))
                    op_file.write('\n')
-
                    
                
                
