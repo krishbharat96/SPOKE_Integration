1f42a734edbfba551959763a2e2301ffd1331745
Krish Bharat <krishbharat96@gmail.com>
2017-11-03 02:39:51 -0500

    Add files via upload

---
+++ Drug_Bank-Extensions/DrugBank_extension.py
@@ -0,0 +1,39 @@
+import xml.etree.ElementTree as ET
+
+o = open('write-db-prot.csv', 'w+')
+
+xml_file = open('full_database.xml', 'r')
+tree = ET.parse(xml_file)
+root = tree.getroot()
+
+ns = '{http://www.drugbank.ca}'
+
+## Schema: DrugBankID|DrugName|UniProtID|DrugBank_ProtID|Category|ActionType
+for drug in root.findall(ns + "drug"):
+    drugbank_id = drug.findtext(ns + "drugbank-id[@primary='true']")
+    drug_name = drug.findtext(ns + "name")
+    type_drug = ['enzyme', 'target', 'carrier', 'transporter']
+    for typ in type_drug:
+        for drug_t in drug.findall(ns + typ + "s/" + ns + typ):
+            act_input = ""
+            for act in drug_t.findall(ns + "actions/" + ns + "action"):
+                if not (act_input == ""):
+                    tmp = act_input + ";" + act.text
+                    act_input = tmp
+                else:
+                    act_input = act.text
+
+            targid = drug_t.findtext(ns + "id")
+            
+            proteinid = ""
+            protein = drug_t.find(ns + "polypeptide")
+            if not (protein == {}) and protein is not None:
+                if "id" in protein.attrib:
+                    proteinid = protein.attrib["id"]
+                
+
+            print drugbank_id + "|" + drug_name + "|" + proteinid + "|" + targid + "|" + typ + "|" + act_input
+            o.write(drugbank_id.encode('utf-8') + "|" + drug_name.encode('utf-8') + "|" + proteinid.encode('utf-8') + "|" + targid.encode('utf-8') + "|" + typ.encode('utf-8') + "|" + act_input.encode('utf-8'))
+            o.write("\n")
+
+
